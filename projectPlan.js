08a8a81e428b785ce5ed5bde0e62bf19d60d4760
ambertrand <alex.m.bertrand@gmail.com>
2021-09-29 10:41:11 -0400

    Add project plans and package json

---
+++ projectPlan.js
@@ -0,0 +1,44 @@
+// User Story //
+
+// 1. As a family member I want to be able to view card designs
+// 2. As a family member I want to be able to enlarge card designs on screen
+// 3. As a family member I want to be able print cards
+// 4. As a family memeber when I print I want some instruction info
+// 5. As a family member I want to be able to be able add to favorites
+// 6. As a family member I want to be able to view favorites/profile page
+
+// Later Dev //
+
+// 1. Add profile/favorites page
+// 2. Build Authentication
+// 3. Other people can add images
+// 4. Upon others uploading pages send approval rights to admins
+// 5. Sort based on theme
+// 6. Email virtual card
+// 7. Copyright for $$
+
+// Tech //
+// React
+// PostgreSQL (pgAdmin)
+// Express
+// Node
+
+// TODO //
+
+// Overall
+
+// 1. Wireframe home, card pages
+// 2. Organize file structure
+
+// Front-End
+
+// 1. Build home page
+// 2. Build card page
+// 3. Build img zoom
+// 4. Build instruction info
+
+// Back-End
+
+// 1. Set up server
+// 2. Build relational models
+// 3. Connect routes
